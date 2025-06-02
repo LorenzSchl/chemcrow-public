356a7383efbe6e71bc691198f3af843d2e25abd0
Lorenz <thesmartly+git@gmail.com>
2025-06-02 20:36:53 +0200

    Some further version bumps.

--- chemcrow/agents/chemcrow.py
+++ chemcrow/agents/chemcrow.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 from langchain import PromptTemplate, chains
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from pydantic import ValidationError
-from rmrkl import ChatZeroShotAgent, RetryAgentExecutor
+from rmrkl.agent import ChatZeroShotAgent
+from rmrkl.executor import RetryAgentExecutor
 
 from .prompts import FORMAT_INSTRUCTIONS, QUESTION_PROMPT, REPHRASE_TEMPLATE, SUFFIX
 from .tools import make_tools
@@ -38,8 +39,8 @@ class ChemCrow:
     def __init__(
         self,
         tools=None,
-        model="gpt-4-0613",
-        tools_model="gpt-3.5-turbo-0613",
+        model="gpt-4.1-mini",
+        tools_model="gpt-4.1-nano",
         temp=0.1,
         max_iterations=40,
         verbose=True,
